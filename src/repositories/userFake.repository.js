f62177ce527d600c3a2b11ee0a6f64296a2c6f3c
Fabian Montaño <fabiantccfa@gmail.com>
2025-08-07 22:56:29 -0600

    add: implement UserFakeRepository with CRUD operations

---
+++ src/repositories/userFake.repository.js
@@ -0,0 +1,48 @@
+const IUserRepository = require('./IUserRepository.js');
+
+class UserFakeRepository extends IUserRepository {
+    constructor() {
+        super();
+        this.users = [];
+        this.nextId = 1;
+    }
+
+    async getById(id) {
+        return this.users.find((user) => user.id === Number(id)) || null;
+    }
+
+    async getAll() {
+        return this.users;
+    }
+
+    async add(user) {
+        user.id = this.nextId++;
+        this.users.push(user);
+
+        return user;
+    }
+
+    async update(updatedUser) {
+        const index = this.users.findIndex(
+            (user) => user.id === updatedUser.id,
+        );
+        if (index === -1) {
+            return null;
+        }
+        this.users[index] = updatedUser;
+
+        return updatedUser;
+    }
+
+    async delete(id) {
+        const index = this.users.findIndex((user) => user.id === Number(id));
+        if (index === -1) {
+            return false;
+        }
+        this.users.splice(index, 1);
+
+        return this.users[index];
+    }
+}
+
+module.exports = UserFakeRepository;
