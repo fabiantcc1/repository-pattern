ea37f6686dff91cbda606ccff5ccdf1977947352
Fabian Montaño <fabiantccfa@gmail.com>
2025-08-07 22:56:41 -0600

    add: implement UserService with CRUD operations

---
+++ src/services/user.services.js
@@ -0,0 +1,49 @@
+const UserRepository = require('../repositories/userFake.repository.js');
+const User = require('../models/user.model.js');
+
+class UserService {
+    constructor() {
+        this.userRepository = new UserRepository();
+    }
+
+    async findOne(id) {
+        const user = await this.userRepository.getById(id);
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        return user;
+    }
+
+    async findAll() {
+        return this.userRepository.getAll();
+    }
+
+    async create(userData) {
+        const user = new User(userData);
+
+        return this.userRepository.add(user);
+    }
+
+    async update(id, userData) {
+        userData.id = id;
+        const userUpdated = this.userRepository.update(userData);
+
+        if (!userUpdated) {
+            throw new Error('User not found');
+        }
+
+        return userUpdated;
+    }
+
+    async delete(id) {
+        const userDeleted = this.userRepository.delete(id);
+
+        if (!userDeleted) {
+            throw new Error('User not found');
+        }
+
+        return userDeleted;
+    }
+}
