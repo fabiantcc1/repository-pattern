3dc9b587e8c98f5ae6e34b38791c96d98a321e26
Fabian Montaño <fabiantccfa@gmail.com>
2025-08-11 23:01:22 -0600

    add: implement user controller with CRUD operations

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,58 @@
+const UserService = require('../services/user.services.js');
+
+const userService = new UserService();
+
+exports.create = async (req, res, next) => {
+    try {
+        const data = req.body;
+        const user = await userService.create(data);
+
+        res.status(201).json(user);
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.getById = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const user = await userService.findOne(id);
+
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.getAll = async (req, res, next) => {
+    try {
+        const users = await userService.findAll();
+
+        res.status(200).json(users);
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.update = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const data = req.body;
+        const user = await userService.update(id, data);
+
+        res.status(200).json(user);
+    } catch (error) {
+        next(error);
+    }
+};
+
+exports.delete = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const user = await userService.delete(id);
+
+        res.status(200).json();
+    } catch (error) {
+        next(error);
+    }
+};
