95598d96df463ca962b6ab7b78525d9d45a8103b
Fabian Montaño <fabiantccfa@gmail.com>
2025-08-14 22:53:45 -0600

    refactor: update UserController to use UserService with UserRepository instance and don't lost "this"

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,58 +1,65 @@
 const UserService = require('../services/user.services.js');
+const UserRepository = require('../repositories/userFake.repository.js');
 
-const userService = new UserService();
-
-exports.create = async (req, res, next) => {
-    try {
-        const data = req.body;
-        const user = await userService.create(data);
-
-        res.status(201).json(user);
-    } catch (error) {
-        next(error);
+class UserController {
+    constructor() {
+        this.service = new UserService(new UserRepository());
     }
-};
 
-exports.getById = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const user = await userService.findOne(id);
+    create = async (req, res, next) => {
+        try {
+            const data = req.body;
+            const user = await this.service.create(data);
 
-        res.status(200).json(user);
-    } catch (error) {
-        next(error);
-    }
-};
+            res.status(201).json(user);
+        } catch (error) {
+            next(error);
+        }
+    };
 
-exports.getAll = async (req, res, next) => {
-    try {
-        const users = await userService.findAll();
+    getById = async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const user = await this.service.findOne(id);
 
-        res.status(200).json(users);
-    } catch (error) {
-        next(error);
-    }
-};
+            res.status(200).json(user);
+        } catch (error) {
+            next(error);
+        }
+    };
 
-exports.update = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const data = req.body;
-        const user = await userService.update(id, data);
+    getAll = async (req, res, next) => {
+        try {
+            const users = await this.service.findAll();
 
-        res.status(200).json(user);
-    } catch (error) {
-        next(error);
-    }
-};
+            res.status(200).json(users);
+        } catch (error) {
+            next(error);
+        }
+    };
 
-exports.delete = async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const user = await userService.delete(id);
+    update = async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const data = req.body;
+            const user = await this.service.update(id, data);
 
-        res.status(200).json(user);
-    } catch (error) {
-        next(error);
-    }
-};
+            res.status(200).json(user);
+        } catch (error) {
+            next(error);
+        }
+    };
+
+    delete = async (req, res, next) => {
+        try {
+            const { id } = req.params;
+            const user = await this.service.delete(id);
+
+            res.status(200).json(user);
+        } catch (error) {
+            next(error);
+        }
+    };
+}
+
+module.exports = UserController;
